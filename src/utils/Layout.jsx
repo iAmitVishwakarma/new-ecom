28ab576604f891aed98c25e226a00a23d8376a0c
iAmitVishwakarma <iam.itvishwakarma3@gmail.com>
2025-06-30 16:16:54 +0530

    Update Layout.jsx
    change direction of login and singup

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -100,10 +100,10 @@ const Layout = () => {
             </>
           ) : (
             <Link
-              to="/login"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              to={localStorage.setItem(isAuthenticated) ? "/login" : "/signup"}
+              className="block px-4 py-2 text-sm text-pink-500 hover:bg-gray-100"
             >
-              Login
+              {localStorage.setItem(isAuthenticated) ? "/login" : "/signup"}
             </Link>
           )}
         </div>
