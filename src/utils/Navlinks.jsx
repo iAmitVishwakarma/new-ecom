9e4c940469cb1fb92046bc63d8a268233c746110
iAmitVishwakarma <iam.itvishwakarma3@gmail.com>
2025-06-30 12:19:26 +0530

    Update Navlinks.jsx
    update logo error and other comments

--- src/utils/Navlinks.jsx
+++ src/utils/Navlinks.jsx
@@ -1,68 +1,3 @@
-//(
-// import React from 'react'
-// import {NavLink  , Routes , Route, useNavigate, Navigate } from "react-router-dom";
-// import Home from '../Home';
-// import Cart from '../Cart';
-// import Profile from '../Profile';
-// import ProductPage from '../ProductPage';
-// import WishlistSec from '../WishlistSec';
-// import { useSelector } from 'react-redux';
-
-// import PaymentCart from '../../components/Cart/Payment/PaymentCart';
-// import BagCart from '../../components/Cart/BagCart';
-// import AddressCart from '../../components/Cart/Address/AddressCart';
-
-
-
-// function Navlinks() {
-//   const {addToCart} = useSelector(state => state.addCart);
-// console.log(addToCart.length);
-
-//   return (
-//     <>
-//     <div className=' flex  items-center  justify-around h-15 mt-2' >
-//     <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><img  className='w-14 ' src="src/assets/logo.webp" /></NavLink>
-//     <div className='flex gap-5 ml-32'>
-//     <NavLink to="/wishlist" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Wishlist</NavLink>
-//     <NavLink to="/cart"  className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
-//   Cart
-//   <span className='relative top-[-10px] left-[-5px] bg-red-500  px-2 py-1 text-xs rounded-full font-bold text-wheat'>
-//     {addToCart.length}
-//   </span>
-// </NavLink>
-// <NavLink to="/profile" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Profile</NavLink>
-// </div>
-// </div>
-//   <hr />
-
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path='/wishlist' element={<div className=' m-auto my-3 px-1 p-2 pb-4 h-full w-10/12 border-2 border-gray-100 rounded shadow-[0px_5px_10px_#0000005e]'  >  <WishlistSec /> </div> } />
-//           <Route path='/products/:name' element={<ProductPage />}  />
-//           <Route path="/cart/"element={<Cart />}> 
-//           <Route path='' element= {<BagCart />} />
-// {
-//   addToCart.length > 0 ?
-//   <> <Route path="payment" element={<PaymentCart />} />
-//      <Route path="address" element={<AddressCart />} />
-// </> : <Route path="*" element={<Navigate to="/" />} />
-
-// }
-
-      
-         
-//      </Route>
-
-//           <Route path="/profile" element={<Profile />} />
-//         </Routes>
-    
-// </>
-//   )
-// }
-
-// export default Navlinks
-//)
-
 import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -75,6 +10,7 @@ import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 // Import your page components
+import Logo from '../../src/assets/logo.webp';
 import Home from '../screen/Home';
 import ProductPage from '../screen/ProductPage';
 import WishlistSec from '../screen/WishlistSec';
@@ -100,8 +36,9 @@ const Navlinks = () => {
        <div className='w-10/12 mx-auto flex justify-between items-center'> <div className="logo">
           <Link to="/">
             <img
-              className="h-10"
-              src="https://iamitvishwakarma.github.io/amit-vishwakarma/assets/img/logo.png"
+              className="h-12"
+              src={Logo}
+             
               alt="logo"
             />
           </Link>
@@ -163,27 +100,6 @@ const Navlinks = () => {
      
       </header>
 
-      {/* <Routes>
-           <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/:name" element={<ProductPage />} />
-        <Route path="/wishlist" element={<WishlistSec />} />
-         <Route path="/cart/"element={<Cart />}> 
-          <Route path='' element= {<BagCart />} />
- {
-  addToCart.length > 0 ?   <> 
-    <Route path="payment" element={<PaymentCart />} /> 
-       <Route path="address" element={<AddressCart />} /> </> 
-       : <Route path="*" element={<Navigate to="/" />} />
- } </Route>
-        <Route path="/profile" element={<Profile />} >
-          <Route path="" element={<Profile activeTab={'profile'} />} />
-          <Route path="addresses" element={<Profile activeTab={'addresses'} />} />
-          <Route path="orders" element={<Profile activeTab={'orders'}/>} />
-        </Route>
-          
-      </Routes> */}
 
        <Routes>
         <Route path="/login" element={<Login />} />
